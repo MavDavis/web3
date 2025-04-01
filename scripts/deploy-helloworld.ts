3ce63e4b6a5c2786f30a1d7ec535f5d25c305e77
Mavdavis <ekwesimarvelous@gmail.com>
2025-04-01 10:57:35 +0100

    wip

---
+++ scripts/deploy-helloworld.ts
@@ -0,0 +1,10 @@
+import "@nomiclabs/hardhat-ethers"
+import { ethers } from "hardhat"
+
+async function deploy() {
+    const HelloWorld = await ethers.getContractFactory("HelloWorld")
+    const hello = await HelloWorld.deploy();
+    await hello.deployed();
+    return hello;
+
+}
