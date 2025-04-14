f31c82a969c6a1928cb9c37070fd0e4134817e40
Mavdavis <ekwesimarvelous@gmail.com>
2025-04-14 20:13:54 +0100

    wip

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -7,7 +7,16 @@ contract Hero {
     function getHeroes() view public returns(uint[] memory){
         return addressToHeroes[msg.sender];
     }
+    function generateRandom() public view returns (uint) {
+    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
+}
     function createHero(Class class) public payable {
         require(msg.value >= 0.05 ether, "Oga, add money");
+         uint[] stats = new uint[](5)
+         stats[0] = 2;
+         stats[1] = 5;
+         stats[2] = 12;
+         stats[3] = 17;
+         stats[4] = 22;
     }
 }
