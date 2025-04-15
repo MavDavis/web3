5e845b4504106997d096d3d0757e7aa8382392cd
Mavdavis <ekwesimarvelous@gmail.com>
2025-04-15 15:47:06 +0100

    wip

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -2,21 +2,39 @@
 pragma solidity ^0.8.0;
 
 contract Hero {
-    enum Class { Mage, Healer, Barbarian }
-    mapping (address => uint[])addressToHeroes;
-    function getHeroes() view public returns(uint[] memory){
+    enum Class {
+        Mage,
+        Healer,
+        Barbarian
+    }
+    mapping(address => uint[]) addressToHeroes;
+    function getHeroes() public view returns (uint[] memory) {
         return addressToHeroes[msg.sender];
     }
     function generateRandom() public view returns (uint) {
-    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
-}
+        return
+            uint(
+                keccak256(abi.encodePacked(block.difficulty, block.timestamp))
+            );
+    }
     function createHero(Class class) public payable {
         require(msg.value >= 0.05 ether, "Oga, add money");
-         uint[] stats = new uint[](5)
-         stats[0] = 2;
-         stats[1] = 5;
-         stats[2] = 12;
-         stats[3] = 17;
-         stats[4] = 22;
+        uint[] memory stats = new uint[](5);
+        stats[0] = 2;
+        stats[1] = 5;
+        stats[2] = 12;
+        stats[3] = 17;
+        stats[4] = 22;
+
+        uint len = 5;
+        uint hero = uint(class);
+        do {
+            uint pos = generateRandom() % len;
+            uint value = generateRandom() % (13 + len)  + 1;
+            hero |= value << stats[pos];
+            len --;
+            stats[pos] = stats[len]; 
+        } while (len > 0);
+        addressToHeroes[msg.sender].push(hero );
     }
 }
