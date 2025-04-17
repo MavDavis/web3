43365992c7719e3602e6628a37a8cd84fd4322b0
Mavdavis <ekwesimarvelous@gmail.com>
2025-04-17 12:20:45 +0100

    wip:hero

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -18,6 +18,21 @@ contract Hero {
                 keccak256(abi.encodePacked(block.difficulty, block.timestamp))
             );
     }
+        function getStrength(uint hero)public pure returns (uint){
+        return (hero >> 2) & 0x1F;
+    }
+        function getHealth(uint hero)public pure returns (uint){
+        return (hero >> 7) & 0x1F;
+    }
+        function getDex(uint hero)public pure returns (uint){
+        return (hero >> 12) & 0x1F;
+    }
+        function getIntellect(uint hero)public pure returns (uint){
+        return (hero >> 17) & 0x1F;
+    }
+        function getMagic(uint hero)public pure returns (uint){
+        return (hero >> 22 ) & 0x1F;
+    }
     function createHero(Class class) public payable {
         require(msg.value >= 0.05 ether, "Oga, add money");
         uint[] memory stats = new uint[](5);
